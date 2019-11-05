efa03b96d73c118cdcd2d933bbcef2a8f1e70d8f
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-11-05 18:52:56 -0500

    Add the slider

--- js/script.js
+++ js/script.js
@@ -13,4 +13,22 @@ $(document).ready(function(){
 		startDelay:995,
 		showCursor: false
 	});
+
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	});
 });
