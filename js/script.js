992ec1ca4169fdb71b61cd05a66d5b37c3bac664
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-11-09 12:23:06 -0500

    Add Counting the number up from 0

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 	$('#slides').superslides({
-		animation:'fade',
-		play:5000,
-		pagination:false
+		animation: 'fade',
+		play: 5000,
+		pagination: false
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "UI/UX Designer."],
-		typeSpeed:70,
-		loop:true,
-		startDelay:995,
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
 		showCursor: false
 	});
 
-
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
-	    items:4,
+	    items: 4,
 	    responsive:{
 	        0:{
 	            items:1
@@ -29,33 +28,45 @@ $(document).ready(function(){
 	            items:3
 	        },
 	        938:{
-	        	items:4
+	            items:4
 	        }
 	    }
 	});
 
+
 	
+
 
 	var skillsTopOffset = $(".skillsSection").offset().top;
 
 	$(window).scroll(function() {
-		if(window.pageYOffset > skillsTopOffset-$(window).height()+200){
 
-				$('.chart').easyPieChart({
-	            easing:'easeInOut',
-	            barColor:'#fff',
-	            trackColor:false,
-	            scaleColor:false,
-	            lineWidth:4,
-	            size:152,
-	            onStep: function(from, to, percent){
-	            	$(this.el).find('.percent').text(Math.round(percent));
-	            }
-	        });
-			
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+
+
 		}
 
+
 	});
+
+	$(".counter").each(function() {
+		var element = $(this);
+		var endVal = parseInt(element.text());
+
+		element.countup(endVal);
+	})
 
 
 });
