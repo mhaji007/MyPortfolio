86db531d0168854276eef42195c4798ac66eba5c
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-11-12 19:03:56 -0500

    Fix navigation bugs

--- js/script.js
+++ js/script.js
@@ -119,9 +119,11 @@ $(document).ready(function() {
 		var body = $("body");
 
 		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
 			body.addClass("fixedNav");
 		}
 		else {
+			body.css("padding-top", 0);
 			body.removeClass("fixedNav");
 		}
 
